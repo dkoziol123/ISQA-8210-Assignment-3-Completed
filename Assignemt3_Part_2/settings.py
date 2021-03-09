53e0c74e9a320d2f38d31cfb63344c5874cbde3d
dkoziol123 <70176289+dkoziol123@users.noreply.github.com>
2021-03-09 08:58:45 -0600

    Update settings.py

--- Assignemt3_Part_2/settings.py
+++ Assignemt3_Part_2/settings.py
@@ -135,3 +135,10 @@ LOGOUT_REDIRECT_URL = 'home'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+LOGIN_REDIRECT_URL = '/'
+
+
